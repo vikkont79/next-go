1a6b7219ed156f1000c9c58422b5fddff3d47a32
vikkont79 <vvikkont79@yandex.ru>
2026-07-09 14:15:51 +0300

    fix: убирает со страницы создания маршрута определение текущего пользователя

--- src/sections/create-trip/ui/CreateTrip/CreateTrip.tsx
+++ src/sections/create-trip/ui/CreateTrip/CreateTrip.tsx
@@ -6,24 +6,11 @@ import styles from './CreateTrip.module.css'
 
 
 const CreateTripPage = async () => {
-  let user = null
-  let error: Error | null = null
-
-  try {
-    user = await getCurrentUser()
-  } catch (error) {
-    console.error('CreateTripPage: Failed to fetch current user', error)
-    error = error instanceof Error ? error : new Error('Unknown auth error')
-  }
-
-  if (error) {
-    return <div className="error">Ошибка загрузки данных пользователя. Невозможно создать маршрут.</div>
-  }
 
   return (
     <main className='wrapper'>
       <h1 className='visually-hidden'>Страница создания маршрута</h1>
-      {user && <UserInfo className={styles.user} />}
+      <UserInfo className={styles.user} />
       <CreateTripForm />
     </main>
   )
